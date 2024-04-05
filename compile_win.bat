f64e16547d96984caab3f4da3546f13da70206eb
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2024-04-05 14:31:32 +0200

    upload base

---
+++ compile_win.bat
@@ -0,0 +1,24 @@
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
+echo WINDOWS > build_logs/build_log_%fullstamp%.txt
+echo ---- >> build_logs/build_log_%fullstamp%.txt
+echo. >> build_logs/build_log_%fullstamp%.txt
+echo BUILD debug_win32 >> build_logs/build_log_%fullstamp%.txt
+scons platform=windows target=template_debug arch=x86_32 >> build_logs/build_log_%fullstamp%.txt
+echo. >> build_logs/build_log_%fullstamp%.txt
+echo BUILD release_win32 >> build_logs/build_log_%fullstamp%.txt
+scons platform=windows target=template_release arch=x86_32 >> build_logs/build_log_%fullstamp%.txt
+echo. >> build_logs/build_log_%fullstamp%.txt
+echo BUILD debug_win64 >> build_logs/build_log_%fullstamp%.txt
+scons platform=windows target=template_debug arch=x86_64 >> build_logs/build_log_%fullstamp%.txt
+echo. >> build_logs/build_log_%fullstamp%.txt
+echo BUILD release_win64 >> build_logs/build_log_%fullstamp%.txt
+scons platform=windows target=template_release arch=x86_64 >> build_logs/build_log_%fullstamp%.txt
+echo. >> build_logs/build_log_%fullstamp%.txt
+
+echo COMPLETE >> build_logs/build_log_%fullstamp%.txt
---
+++ extension_src/register_types.h
@@ -0,0 +1,13 @@
+#ifndef GDEXAMPLE_REGISTER_TYPES_H
+#define GDEXAMPLE_REGISTER_TYPES_H
+
+#include "gdexample.h"
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_example_module(ModuleInitializationLevel p_level);
+void uninitialize_example_module(ModuleInitializationLevel p_level);
+
+#endif // GDEXAMPLE_REGISTER_TYPES_H
