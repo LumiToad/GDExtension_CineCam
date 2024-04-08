680f0fe147f42716e1c35d07c8daa972a9c75ae6
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2024-04-08 22:38:48 +0200

    added build scripts and configs for linux build
    - requires virtual machine setup (not included)

--- compile_win.bat
+++ compile_win.bat
@@ -5,20 +5,20 @@ set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
-echo WINDOWS > build_logs/build_log_%fullstamp%.txt
-echo ---- >> build_logs/build_log_%fullstamp%.txt
-echo. >> build_logs/build_log_%fullstamp%.txt
-echo BUILD debug_win32 >> build_logs/build_log_%fullstamp%.txt
-scons platform=windows target=template_debug arch=x86_32 >> build_logs/build_log_%fullstamp%.txt
-echo. >> build_logs/build_log_%fullstamp%.txt
-echo BUILD release_win32 >> build_logs/build_log_%fullstamp%.txt
-scons platform=windows target=template_release arch=x86_32 >> build_logs/build_log_%fullstamp%.txt
-echo. >> build_logs/build_log_%fullstamp%.txt
-echo BUILD debug_win64 >> build_logs/build_log_%fullstamp%.txt
-scons platform=windows target=template_debug arch=x86_64 >> build_logs/build_log_%fullstamp%.txt
-echo. >> build_logs/build_log_%fullstamp%.txt
-echo BUILD release_win64 >> build_logs/build_log_%fullstamp%.txt
-scons platform=windows target=template_release arch=x86_64 >> build_logs/build_log_%fullstamp%.txt
-echo. >> build_logs/build_log_%fullstamp%.txt
+echo WINDOWS > build_logs/windows/windows/build_log_%fullstamp%.txt
+echo ---- >> build_logs/windows/build_log_%fullstamp%.txt
+echo. >> build_logs/windows/build_log_%fullstamp%.txt
+echo BUILD debug_win32 >> build_logs/windows/build_log_%fullstamp%.txt
+scons platform=windows target=template_debug arch=x86_32 >> build_logs/windows/build_log_%fullstamp%.txt
+echo. >> build_logs/windows/build_log_%fullstamp%.txt
+echo BUILD release_win32 >> build_logs/windows/build_log_%fullstamp%.txt
+scons platform=windows target=template_release arch=x86_32 >> build_logs/windows/build_log_%fullstamp%.txt
+echo. >> build_logs/windows/build_log_%fullstamp%.txt
+echo BUILD debug_win64 >> build_logs/windows/build_log_%fullstamp%.txt
+scons platform=windows target=template_debug arch=x86_64 >> build_logs/windows/build_log_%fullstamp%.txt
+echo. >> build_logs/windows/build_log_%fullstamp%.txt
+echo BUILD release_win64 >> build_logs/windows/build_log_%fullstamp%.txt
+scons platform=windows target=template_release arch=x86_64 >> build_logs/windows/build_log_%fullstamp%.txt
+echo. >> build_logs/windows/build_log_%fullstamp%.txt
 
-echo COMPLETE >> build_logs/build_log_%fullstamp%.txt
+echo COMPLETE >> build_logs/windows/build_log_%fullstamp%.txt
